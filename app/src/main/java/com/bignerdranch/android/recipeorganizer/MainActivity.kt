98e558e8fc084f3994220ec9f3621b6d2e6d40c5
unknown <taylork1201@yahoo.com>
2023-11-25 19:00:56 -0800

    Adding front-end

---
+++ app/src/main/java/com/bignerdranch/android/recipeorganizer/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.recipeorganizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.recipeorganizer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.Home -> replaceFragment(Home())
+                R.id.CreateRecipe -> replaceFragment(CreateRecipe())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
