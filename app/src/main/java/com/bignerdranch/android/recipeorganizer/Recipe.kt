ee4e2ba9ed14b8f216c59623d1fd182875f2c80f
julia463 <54084784+julia463@users.noreply.github.com>
2023-11-26 22:42:53 -0800

    updating package structure

---
+++ app/src/main/java/com/bignerdranch/android/recipeorganizer/Recipe.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.recipeorganizer
+
+import java.util.UUID
+
+data class Recipe(
+    val id: UUID,
+    val name: String,
+    val mealType: String,
+    val prepTime: Int,
+    val servingSize: Int,
+    val ingredients: List<String>,
+    val steps: List<String>,
+    val similarRecipes: List<String>,
+    val dietaryAccomodations: List<String>,
+)
---
+++ app/src/main/java/com/bignerdranch/android/recipeorganizer/RecipeDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.recipeorganizer
+
+
+import android.app.Fragment
+import android.os.Bundle
+import java.util.UUID
+
+class RecipeDetailFragment: Fragment() {
+
+    private lateinit var recipe: Recipe
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        recipe = Recipe(
+            id = UUID.randomUUID(),
+            name="",
+            mealType="breakfast",
+            prepTime=0,
+            servingSize=4,
+            ingredients=listOf<String>(),
+            steps=listOf<String>(),
+            similarRecipes=listOf<String>(),
+            dietaryAccomodations=listOf<String>()
+
+        )
+    }
+
+}
