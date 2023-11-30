bfe86bd67ed8363e3d9f73f9ab5f5fc350d33fb4
Winter4Life <taylork1201@yahoo.com>
2023-11-30 05:39:12 -0800

    deleted files/changed layout/fixed some buttons

---
+++ app/src/main/java/com/bignerdranch/android/recipeorganizer/RecipeListFragment.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.recipeorganizer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class RecipeListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
